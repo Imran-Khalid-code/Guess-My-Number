5ea68c90d6e698a75f1db94ed0aa8c3e1a124eda
imran <ikhalidf7@gmail.com>
2021-12-12 21:32:26 +0000

    conditionl statements refactored

--- script.js
+++ script.js
@@ -21,54 +21,76 @@ document.querySelector('.guessNumber').addEventListener('click', () => {
 	console.log(userGuess, typeof userGuess)
 
 
-//function for too high or to low : then convert into a ternary operator:
+//function to distill the amount of messages - refactoring
 
+const displayMessage = (correctNumber) => {
+	document.querySelector('.correctNumber').textContent = correctNumber
+}
 
 
 
 ///first need to check if there is a value
 if(!userGuess){
-	document.querySelector('.correctNumber').textContent = '🥺 No number!'
+	// document.querySelector('.correctNumber').textContent = '🥺 No number!'
+	displayMessage('🥺 No number!')
 
 //when player wins	
 }else if(userGuess === secretGuess){
 	document.querySelector('.correctNumber').textContent = '😆  Congratulations!'
+	displayMessage('😆  Congratulations!')
 	document.querySelector('.gameResult').textContent = secretGuess
 	document.querySelector('body').style.backgroundColor = 'pink'
 	document.querySelector('.gameResult').style.width = '30rem'
-	
+
 	
 	if(gameScore > highScore){
 		highScore = gameScore
 		document.querySelector('.highScore').textContent = highScore
+	
 	}
-	
-	
+//clean up and refactor if duplicate if else statements
 
-}else if(userGuess > secretGuess ){
-	if(gameScore > 1){
-		document.querySelector('.correctNumber').textContent = '🤭 Too High!'
+	}else if(userGuess !== secretGuess){
+	if(gameScore > 1) { 
+		document.querySelector('.correctNumber').textContent =
+		userGuess > secretGuess ? '🤭 Too High!' : '🤭 Too Low!'
 		gameScore --
-		document.querySelector('.score').textContent = gameScore 	
-	}else {
-		document.querySelector('.correctNumber').textContent = '🥺 You Lost!'
-		document.querySelector('.score').textContent = 0
-	}
+		document.querySelector('.score').textContent = gameScore 
 
-//when guess is to low	
-}else if(userGuess < secretGuess ){
-	
-	if(gameScore > 1){
-		document.querySelector('.correctNumber').textContent = '🤭 Too Low!'
-		gameScore --
-		document.querySelector('.score').textContent = gameScore 	
-	}else {
-		document.querySelector('.correctNumber').textContent = '🥺 You Lost!'
-		document.querySelector('.score').textContent = 0
+	}  else {
+			// document.querySelector('.correctNumber').textContent = '🥺 You Lost!'
+			displayMessage('😆  Congratulations!')
+			document.querySelector('.score').textContent = 0
+		}
 	}
-}
 
 })
+
+//when guess is to high
+// }else if(userGuess > secretGuess ){
+// 	if(gameScore > 1){
+// 		document.querySelector('.correctNumber').textContent = '🤭 Too High!'
+// 		gameScore --
+// 		document.querySelector('.score').textContent = gameScore 	
+// 	}else {
+// 		document.querySelector('.correctNumber').textContent = '🥺 You Lost!'
+// 		document.querySelector('.score').textContent = 0
+// 	}
+
+// //when guess is to low	
+// }else if(userGuess < secretGuess ){
+	
+// 	if(gameScore > 1){
+// 		document.querySelector('.correctNumber').textContent = '🤭 Too Low!'
+// 		gameScore --
+// 		document.querySelector('.score').textContent = gameScore 	
+// 	}else {
+// 		document.querySelector('.correctNumber').textContent = '🥺 You Lost!'
+// 		document.querySelector('.score').textContent = 0
+// 	}
+// }
+
+
 
 
 //outside of the logic do a reset with an annoymous function:
@@ -91,13 +113,3 @@ document.querySelector('.playAgain').addEventListener('click',()=>{
 })
 
 
-//problem need to reduce highscore if guess is incorrect by one each time & then if reaches 0 change message to stay game over
-
-//problem is stop the number going below zero - knock this one first - possible solution either change it to onclick and make a posiitive and negative button and change its value that way or set a range on the input value.
-
-//problem score also need to log highScore on every game finish
-
-//problem need to reset the values on the end, so when it wins = reset,when it gets to the cut off point it resets.
-
-
-//make project a little bit more complex by adding a timeout/set interval logic.- delay the message slighlt to build up the suspense.
